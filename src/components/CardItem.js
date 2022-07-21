0872ef96416b004f83ef5f2db269034bb72b4b21
Shammi Anand <54147419+ShammiAnand@users.noreply.github.com>
2022-07-22 03:24:00 +0530

    added all UI components

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { PieChart } from "react-minimal-pie-chart";
 // Import react-circular-progressbar module and styles
 import {
   CircularProgressbar,
@@ -17,13 +18,13 @@ export const CardItem = ({ user }) => {
         borderRadius: "1rem",
         backgroundColor: "#1E262F",
         display: "flex",
-        justifyContent: "start",
+        justifyContent: "space-between",
         alignItems: "center",
       }}
     >
       <img
         src={process.env.PUBLIC_URL + "/user_photo.png"}
-        style={{ width: "2.5rem", height: "2.5rem", marginLeft: "0.5rem" }}
+        style={{ width: "2.5rem", height: "2.5rem", marginLeft: "1rem" }}
       />
       <div
         style={{ marginLeft: "1rem", display: "flex", flexDirection: "column" }}
@@ -34,7 +35,7 @@ export const CardItem = ({ user }) => {
           {user.email}{" "}
         </span>
       </div>
-      <div style={{ width: "4rem", marginLeft: "2rem" }}>
+      <div style={{ width: "4rem", marginLeft: "3rem" }}>
         <CircularProgressbarWithChildren
           value={`${(user.stepsWalked / user.stepsTarget) * 100}`}
         >
@@ -94,11 +95,136 @@ export const CardItem = ({ user }) => {
 
       <div
         style={{
+          marginLeft: "3rem",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          color: "white",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={process.env.PUBLIC_URL + "/checked.png"} />
+          <span
+            style={{
+              fontSize: "1rem",
+              fontWeight: "800",
+              marginLeft: "0.5rem",
+            }}
+          >
+            15 Oct
+          </span>
+        </div>
+
+        <div
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={process.env.PUBLIC_URL + "/due.png"} />
+          <span
+            style={{
+              fontSize: "1rem",
+              fontWeight: "800",
+              marginLeft: "0.5rem",
+            }}
+          >
+            22 Oct
+          </span>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "80%",
+          width: "3%",
+          borderRadius: "1rem",
+          backgroundColor: "#101317",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginLeft: "0.7rem",
+        }}
+      >
+        <img src={process.env.PUBLIC_URL + "/right.png"} />
+      </div>
+      <div style={{ width: "4rem", marginLeft: "3rem" }}>
+        <PieChart
+          data={[
+            { title: "One", value: 10, color: "#E38627" },
+            { title: "Two", value: 15, color: "#C13C37" },
+            { title: "Three", value: 20, color: "#6A2135" },
+          ]}
+        />
+      </div>
+
+      <div
+        style={{
+          marginLeft: "1rem",
+          color: "white",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <PlusCircleOutlined style={{ fontSize: "1.5rem" }} />
+        <div
+          style={{
+            marginTop: "0.2rem",
+            marginBottom: "0.2rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ fontSize: "1.2rem", fontWeight: "800" }}>
+            {user.stepsTarget / 1000}k
+          </span>
+          <span
+            style={{
+              fontSize: "0.8rem",
+              fontWeight: "300",
+              marginTop: "-0.2rem",
+            }}
+          >
+            target
+          </span>
+        </div>
+        <MinusCircleOutlined style={{ fontSize: "1.5rem" }} />
+      </div>
+      <div
+        style={{
+          height: "80%",
+          width: "3%",
+          borderRadius: "1rem",
+          backgroundColor: "#101317",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginLeft: "0.7rem",
+        }}
+      >
+        <img src={process.env.PUBLIC_URL + "/right.png"} />
+      </div>
+
+      <div
+        style={{
           backgroundColor: "#36F5C7",
           height: "2.5rem",
           width: "2.5rem",
           borderRadius: "1rem",
-          marginLeft: "2rem",
+          marginLeft: "3rem",
+          marginRight: "1rem",
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
